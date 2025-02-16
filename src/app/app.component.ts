910bce4572597d583904b83d90f92ef9cecd044b
Ankit Kumar <kumarankit0520@gmail.com>
2025-02-17 02:34:15 +0530

    `Added app-homestays-carousel and app-adventures components to app.component.html and updated imports in app.component.ts`ng s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './home/nav-bar/nav-bar.component';
-import { HeroSectionComponent } from './home/hero-section/hero-section.component';
-import { VillageCarouselComponent } from './home/village-carousel/village-carousel.component';
-import { HomestaysCarouselComponent } from './home/homestays-carousel/homestays-carousel.component';
-import { AdventuresComponent } from './home/adventures/adventures.component';
-import { UpcomingEventsComponent } from './home/upcoming-events/upcoming-events.component';
-import { ProductsCarouselComponent } from './home/products-carousel/products-carousel.component';
+import { HomeMainComponent } from './home/home-main/home-main.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBarComponent,HeroSectionComponent,VillageCarouselComponent,HomestaysCarouselComponent,
-    AdventuresComponent,UpcomingEventsComponent,ProductsCarouselComponent
-  ],
+  imports: [RouterOutlet,HomeMainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/home/home-main/home-main.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
+import { AdventuresComponent } from '../adventures/adventures.component';
+import { HomestaysCarouselComponent } from '../homestays-carousel/homestays-carousel.component';
+import { ProductsCarouselComponent } from '../products-carousel/products-carousel.component';
+import { UpcomingEventsComponent } from '../upcoming-events/upcoming-events.component';
+import { VillageCarouselComponent } from '../village-carousel/village-carousel.component';
+
+@Component({
+  selector: 'app-home-main',
+  templateUrl: './home-main.component.html',
+  styleUrls: ['./home-main.component.css'],
+  imports:[NavBarComponent,HeroSectionComponent,AdventuresComponent,HomestaysCarouselComponent,
+    ProductsCarouselComponent,UpcomingEventsComponent,VillageCarouselComponent]
+})
+export class HomeMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
